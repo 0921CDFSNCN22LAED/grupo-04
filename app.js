fe8cf17298305677d637d85cb5e6a884ee515c51
Ayelen <ayealvo@gmail.com>
2021-10-22 10:33:25 -0300

    agrego mensaje en la consola del servidor funcionando

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000), () => 
-  console.log('Servidor corriendo en puerto 3000');
+
+app.listen(3000, () =>
+ console.log('Servidor corriendo en puerto 3000')
+);
 
 app.use(express.static(__dirname + '/public'));
 
