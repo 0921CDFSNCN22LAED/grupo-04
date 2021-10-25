64bbe19e8f2be80332bc9a4327dda1aa4d406293
Tomás Salvatore <tomisalvatore@gmail.com>
2021-10-25 19:17:17 -0300

    Primer maquetado de pagina de login

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.listen(3000, () =>
- console.log('Servidor corriendo en puerto 3000')
-);
+app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/home.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/login.html'));
 });
