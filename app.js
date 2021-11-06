7afff1f4aa3e04bea7b097d1f5dd4eedcc87ed50
Ayelen <ayealvo@gmail.com>
2021-11-06 18:28:31 -0300

    maquetado menu responsive

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/home.html'));
 });
 
+app.get('/market', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/market.html'));
+});
+
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/register.html'));
 });
