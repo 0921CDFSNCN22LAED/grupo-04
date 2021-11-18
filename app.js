e17f3c7d6f16c28815ebf46cd167cbd2cfc6bc86
Tomás Salvatore <tomisalvatore@gmail.com>
2021-11-18 19:41:48 -0300

    Agregando EJS

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
   console.log('Servidor funcionando');
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,17 +1,15 @@
-const path = require('path');
-
 const controlador = {
   home: (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/home.html'));
+    res.render('home');
   },
   login: (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/login.html'));
+    res.render('login');
   },
   register: (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/register.html'));
+    res.render('register');
   },
   market: (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/market.html'));
+    res.render('market');
   },
 };
 
