299b2d1e296da897f6baa027a61a09b9bcef58c0
ayeAlvo <ayealvo@gmail.com>
2021-11-22 20:41:11 -0300

    vistas dinamicas en el market (fallo en el modal)
    no pude hacer que funcione en el modal tambien

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,3 +1,38 @@
+const listCards = [
+  {
+    id: "1",
+    name: "Ryu",
+    desc: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    date: "14/11/2021",
+    price: "0.5",
+    img: "/images/calamardos-nft/ryu.jpg",
+  },
+  {
+    id: "2",
+    name: "Sailor Moon",
+    desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    date: "14/11/2021",
+    price: "0.5",
+    img: "/images/calamardos-nft/sailor_moon1.jpg",
+  },
+  {
+    id: "3",
+    name: "Sasuke",
+    desc: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    date: "14/11/2021",
+    price: "0.5",
+    img: "/images/calamardos-nft/sasukeFinal.jpg",
+  },{
+    id: "4",
+    name: "Ryu2",
+    desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    date: "14/11/2021",
+    price: "0.5",
+    img: "/images/calamardos-nft/ryu.jpg",
+  }
+];
+
+
 const controlador = {
   home: (req, res) => {
     res.render('home');
@@ -9,7 +44,9 @@ const controlador = {
     res.render('register');
   },
   market: (req, res) => {
-    res.render('market');
+    res.render('market',{
+      listCards: listCards
+    });
   },
   edit: (req, res) => {
     res.render('admin-edit-products');
