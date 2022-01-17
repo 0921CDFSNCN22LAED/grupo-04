7a9cfe56a9c847b612cd7643e601a4e38a5a85e9
ayeAlvo <ayealvo@gmail.com>
2022-01-17 19:17:19 -0300

    validacion contraseñas iguales
    agrego validacion al register para que valide que el campo de contraseña y repetir contraseña sean iguales

--- middlewares/userValidations.js
+++ middlewares/userValidations.js
@@ -1,15 +1,20 @@
 const { body } = require('express-validator');
 
 module.exports = [
-  body('name').notEmpty().withMessage('Debes ingresar un nombre'),
+  body('name').notEmpty().withMessage('Debes ingresar un nombre').bail()
+    .isLength({ min: 3 }).withMessage('Debe ser minimo 3 caracteres'),
   body('userName').notEmpty().withMessage('Debes ingresar un Nickname/Username')
-    .isLength({ min: 4 }).withMessage('Debes ser minimo de 4 caracteres'),
+    .isLength({ min: 3 }).withMessage('Debe ser minimo 3 caracteres'),
   body('email').notEmpty().withMessage('Debes ingresar un email').bail()
     .isEmail().withMessage('Debes ingresar un email valido'),
   body('password').notEmpty().withMessage('Debes ingresar una contraseña').bail()
     .isLength({ min: 5 }).withMessage('La contraseña debe tener al menos 5 caracteres'),
-  body('passwordCheck').notEmpty().withMessage('Debes repetir la contraseña').bail(),
-  // .equals('password').withMessage('Las contraseñas deben coincidir'),
+  body('passwordCheck').custom((value, { req }) =>{ 
+    if(value !== req.body.password){
+    throw new Error('Las contraseñas deben ser iguales')
+    }
+    return true;
+}),
   body('avatar').custom((value, { req }) => {
     let file = req.file;
     if(!file){
