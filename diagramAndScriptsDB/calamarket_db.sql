e54fedf2e4c5a280c81e8e43edce998558a3a66f
ayeAlvo <ayealvo@gmail.com>
2022-01-22 23:08:49 -0300

    carpeta de diagrama y script de estructura DB
    diagramas de la base de datos, y script para generar la estructura

---
+++ diagramAndScriptsDB/calamarket_db.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-01-2022 a las 03:00:26
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `calamarket_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cards`
+--
+
+CREATE TABLE `cards` (
+  `id` int(6) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `description` varchar(60) NOT NULL,
+  `rating` int(3) NOT NULL,
+  `image` varchar(60) NOT NULL,
+  `create_data` datetime NOT NULL DEFAULT current_timestamp(),
+  `price` int(11) NOT NULL,
+  `category_id` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `category_card`
+--
+
+CREATE TABLE `category_card` (
+  `id` int(11) NOT NULL,
+  `name` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `karts`
+--
+
+CREATE TABLE `karts` (
+  `id` int(6) NOT NULL,
+  `items` int(30) NOT NULL,
+  `total` int(50) NOT NULL,
+  `products_id` varchar(50) NOT NULL,
+  `usuario_id` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `product_kart`
+--
+
+CREATE TABLE `product_kart` (
+  `id` int(6) NOT NULL,
+  `kart_id` int(6) NOT NULL,
+  `card_id` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(6) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `avatar` varchar(11) NOT NULL,
+  `password` varchar(11) NOT NULL,
+  `role` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cards`
+--
+ALTER TABLE `cards`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category` (`category_id`);
+
+--
+-- Indices de la tabla `category_card`
+--
+ALTER TABLE `category_card`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `karts`
+--
+ALTER TABLE `karts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `uduario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `product_kart`
+--
+ALTER TABLE `product_kart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_kart` (`kart_id`),
+  ADD KEY `card_id` (`card_id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cards`
+--
+ALTER TABLE `cards`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `category_card`
+--
+ALTER TABLE `category_card`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `karts`
+--
+ALTER TABLE `karts`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `product_kart`
+--
+ALTER TABLE `product_kart`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cards`
+--
+ALTER TABLE `cards`
+  ADD CONSTRAINT `category` FOREIGN KEY (`category_id`) REFERENCES `category_card` (`id`);
+
+--
+-- Filtros para la tabla `karts`
+--
+ALTER TABLE `karts`
+  ADD CONSTRAINT `user_kart` FOREIGN KEY (`usuario_id`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `product_kart`
+--
+ALTER TABLE `product_kart`
+  ADD CONSTRAINT `product_kart` FOREIGN KEY (`kart_id`) REFERENCES `karts` (`id`),
+  ADD CONSTRAINT `product_kart_ibfk_1` FOREIGN KEY (`card_id`) REFERENCES `cards` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
