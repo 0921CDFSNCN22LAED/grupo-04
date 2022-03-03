ef76612976bd0a719fa7be885743e7335dfabcf3
ayeAlvo <ayealvo@gmail.com>
2022-03-03 16:34:01 -0300

    validaciones front login funcionando
    falta arreglar el border success en el password

--- website/public/js/validationsFormLogIn.js
+++ website/public/js/validationsFormLogIn.js
@@ -1,58 +1,75 @@
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
+window.addEventListener('load', () => {
 
-const ulErrors = document.querySelector('.errors');
-const inputs = document.querySelector('.inputs');
-const labelEmail = document.querySelector('.labelEmail');
-const labelPassword = document.querySelector('.labelPassword');
+  const form = document.querySelector('#form');
+  const email = document.querySelector('#email');
+  const password = document.querySelector('#password');
+  const ulErrors = document.querySelector('.errors');
+  const inputs = document.querySelectorAll('input');
+  const labelEmail = document.querySelector('.labelEmail');
+  const inputEmail = document.querySelector('.input-email');
+  const labelPassword = document.querySelector('.labelPassword');
+  const inputPassword = document.querySelector('input.input-password');
 
-const validarFormulario = (e) => {
-  switch (e.target.name) {
-    case 'email':
-      if (email.value.lenght <= 1 || email.value.lenght > 20) {
-        labelEmail.innerText = "El email debe ya estar en la base";
-        labelEmail.classList.add('is-invalid');
-        labelEmail.classList.remove('is-valid');
-      } else {
-        labelEmail.innerText = "Email";
-        labelEmail.classList.add('is-valid');
-        labelEmail.classlist.remove('is-invalid');
-      }
-      break
-    case 'password':
-      if (password.value.lenght < 1) {
-        labelPassword.innerText = "Debes escribir una contraseña";
-        labelPassword.classList.add('is-invalid');
-        labelPassword.classList.remove('is-valid');
-      } else {
-        labelPassword.innerText = "Contraseña";
-        labelPassword.classList.add('is-valid');
-        labelPassword.classlist.remove('is-invalid');
-      }
-      break
+  const validarFormulario = (e) => {
+    switch (e.target.name) {
+      case 'email':
+        if (email.value.length <= 3) {
+          labelEmail.innerText = "Debes ingresar un email";
+          labelEmail.classList.add('text-danger');
+          inputEmail.classList.remove('border', 'border-success');
+        } else {
+          labelEmail.innerText = "";
+          labelEmail.classList.remove('text-danger');
+          inputEmail.classList.add('border', 'border-success');
+        }
+        break;
+      case 'password':
+        if (password.value.length <= 1) {
+          labelPassword.innerText = "Debes escribir una contraseña";
+          labelPassword.classList.add('text-danger');
+          inputPassword.classList.remove('border', 'border-success');
+        } else {
+          labelPassword.innerText = "";
+          labelPassword.classlist.remove('text-danger');
+          // no funciona el border success, no se porque
+          inputPassword.classList.add('border', 'border-success');
+        }
+        break;
+    }
   }
-}
-inputs.forEach(input => {
-  input.addEventLinstener('keyup', validarFormulario);
-  input.addEventLinstener('blur', validarFormulario);
-});
-formulario.addEventLinstener('submit', (e) => {
-  let erorrs = [];
-  if (email.value == '') {
-    erorrs.push('El campo Email no puede quedar vacio');
-  }
-  if (password.value == '') {
-    erorrs.push('El campo Password no puede quedar vacio');
-  }
-  if (errors.lenght > 0) {
-    e.preventDeFault();
-    errors.forEach(error => {
-      ulErrors.classlist.add('alert-warning');
-      ulErrors.innerHTML += `<li>${error}<li>`;
-      console.log(error)
-    })
-  } else {
-    alert('El usuario se logueo correctamente')
-  }
-
+  
+  inputs.forEach(input => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+  });
+  
+  
+  form.addEventListener('submit', (e) => {
+    let errors = [];
+    if (email.value == '') {
+      errors.push('El campo Email no puede quedar vacio');
+    }
+    if (password.value == '') {
+      errors.push('El campo Password no puede quedar vacio');
+    }
+  
+    console.log(errors.length);
+    console.log(errors);
+  
+    if (errors.length > 0) {
+      e.preventDefault();
+      errors.forEach(error => {
+        ulErrors.classList.add('alert-warning');
+        ulErrors.innerHTML += `<li>${error}<li>`;
+        // hacer que no se sumen devuelta los errores al ul
+        // y desaparezca el que esta valdidado
+      })
+    } else {
+      alert('El usuario paso las validaciones del front')
+    }
+  
+  })
 })
+
+
+
