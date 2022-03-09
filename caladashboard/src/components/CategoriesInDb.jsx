2b119f8b419aa37e7a7cc47effebf603ceb22281
ayeAlvo <ayealvo@gmail.com>
2022-03-08 22:51:51 -0300

    vista dashboard categories
    Co-Authored-By: Tomás Salvatore <44672280+tomassalvatore@users.noreply.github.com>
    Co-Authored-By: Abe <91475491+Abe1406@users.noreply.github.com>
    Co-Authored-By: Nicolas Di Perna <91568704+NicoDiPerna@users.noreply.github.com>

--- caladashboard/src/components/CategoriesInDb.jsx
+++ caladashboard/src/components/CategoriesInDb.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Category from "./Category";
 
 function GenresInDb() {
+  const [categories, setCategories] = useState([])
+
+  const getCategoriesData = async () => {
+    const response = await fetch('http://localhost:3001/api/categories');
+    const data = await response.json();
+    console.log(data);
+    setCategories(data.data);
+  };
+
+  useEffect(() => {
+    getCategoriesData()
+  }, [])
+
   return (
+
     <div className="col-lg-6 mb-4">
       <div className="card shadow mb-4">
         <div className="card-header py-3">
@@ -11,56 +26,12 @@ function GenresInDb() {
         </div>
         <div className="card-body">
           <div className="row">
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Acción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Animación</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Aventura</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Ciencia Ficción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Comedia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Documental</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Drama</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Fantasia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Infantiles</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Musical</div>
-              </div>
-            </div>
+          {
+             categories.map(category =>{
+              return  <Category {...category}  key={category.id} />
+             })
+         }
+
           </div>
         </div>
       </div>
