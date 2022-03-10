15099ab605d1d38c07ba8bade41b47fa97867a35
Tomás Salvatore <tomisalvatore@gmail.com>
2022-03-10 00:23:18 -0300

    fixing imports

--- caladashboard/src/components/Chart.jsx
+++ caladashboard/src/components/Chart.jsx
@@ -1,22 +1,4 @@
 import React from 'react';
-import ChartRow from './ChartRow';
-
-let tableRowsData = [
-  {
-    Title: 'Billy Elliot ',
-    Length: '123',
-    Rating: '5',
-    Categories: ['Drama', 'Comedia'],
-    Awards: 2,
-  },
-  {
-    Title: 'Alicia en el país de las maravillas',
-    Length: '142',
-    Rating: '4.8',
-    Categories: ['Drama', 'Acción', 'Comedia'],
-    Awards: 3,
-  },
-];
 
 const Chart = (props) => {
   console.log(props.data);
